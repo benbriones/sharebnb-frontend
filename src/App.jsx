362c5e49f3cda68558c49af6da4120aebdb310c0
Ben Briones <benbriones50@gmail.com>
2024-04-18 10:20:38 -0700

    app setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import './App.css';
 
 
-/** Component for entire page.
+/** Component for ShareBnB App.
  *
  * Props: none
  * State: none
@@ -11,28 +11,10 @@ import './App.css';
 */
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  function incrCount() {
-    setCount(count => count + 1);
-  }
 
   return (
     <div className="App">
-      <main>
-        <img src={logo} className="App-logo" alt="Rithm" />
-        <h1>Rithm React Starter</h1>
-        <p>
-          <button
-              className="btn btn-primary"
-              onClick={incrCount}>
-            Clicked: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>src/App.jsx</code>
-        </p>
-      </main>
     </div>
   );
 };
